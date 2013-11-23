c8ebc654530ece300c2ec19467d99bd967cffcdd
Alex Rezit <AlexRezit@gmail.com>
2013-11-23 13:03:50 +0800

    Add node support

--- V2EX_LinkUnifier.safariextension/LinkUnifier.js
+++ V2EX_LinkUnifier.safariextension/LinkUnifier.js
@@ -5,8 +5,8 @@ function replaceAnchors(topicContent) {
     var anchors = Array.prototype.slice.call(topicContent.getElementsByTagName("a"));
     console.log(anchors);
     anchors.forEach(function(anchor) {
-        anchor.innerHTML = anchor.innerHTML.replace(/(http|https):\/\/(|.*\.)v2ex\.com(\/t\/\d+)/i, "$3");
-        anchor.href = anchor.href.replace(/(http|https):\/\/(|.*\.)v2ex\.com(\/t\/\d+)/i, "$3");
+        anchor.innerHTML = anchor.innerHTML.replace(/(http|https):\/\/(|.*\.)v2ex\.com(\/t\/\d+|\/go\/\w+)/i, "$3");
+        anchor.href = anchor.href.replace(/(http|https):\/\/(|.*\.)v2ex\.com(\/t\/\d+|\/go\/\w+)/i, "$3");
     });
 }
 
